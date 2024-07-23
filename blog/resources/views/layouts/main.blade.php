b6fa4155a7e761debd192db927fe1be683ac3d90
Swaye Chateau <swayec@gmail.com>
2024-07-23 14:35:36 -0700

    removed build styles and added manually

--- blog/resources/views/layouts/main.blade.php
+++ blog/resources/views/layouts/main.blade.php
@@ -8,7 +8,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     @yield('styles')
     <style>
 		@media (prefers-color-scheme: dark) {
